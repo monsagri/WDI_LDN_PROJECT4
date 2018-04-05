284baa57aab5fe9367a86ad73b7050cd3a7029bb
Fabian Feldberg <feldbergf@gmail.com>
2018-04-05 12:36:44 +0100

    starting on frontend basics

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'bulma';
+
+import Title from './assets/styledComponents/Title';
+
 class App extends React.Component {
   render() {
     return (
-      <h1>WDI32 Project 4</h1>
+      <Title>Rivae</Title>
     );
   }
 }
