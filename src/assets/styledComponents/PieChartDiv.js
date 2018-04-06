4ba088224dd28f1635d2cf26f8cff06399ba7ce7
Fabian Feldberg <feldbergf@gmail.com>
2018-04-06 10:56:29 +0100

    added Victory for visualization, made pie charts for category and payee

---
+++ src/assets/styledComponents/PieChartDiv.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const PieChartDiv = styled.div`
+  width: 600px;
+  height: 600px;
+  svg {
+    padding-left: 50px;
+    padding-right: 50px;
+  }
+`;
+
+export default PieChartDiv;
--- src/components/Rivae/UserRoute.js
+++ src/components/Rivae/UserRoute.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import { VictoryPie, VictoryLabel } from 'victory';
+
+import PieChartDiv from '../../assets/styledComponents/PieChartDiv';
+import FlexRowDiv from '../../assets/styledComponents/FlexRowDiv';
+
+
 
 class UserRoute extends React.Component{
   state = {
@@ -10,9 +16,13 @@ class UserRoute extends React.Component{
       email: '',
       transactions: [],
       username: '',
-      balance: ''
+      balance: '',
+      spendingByCategory: [],
+      spendingByPayee: []
     }
   }
+
+
 
   componentDidMount() {
     axios.get(`/api/users/${this.props.match.params.id}`)
@@ -20,10 +30,89 @@ class UserRoute extends React.Component{
   }
 
   render() {
+    const spendingPieData =
+    Object.entries(this.state.user.spendingByCategory)
+      .map(entry => Object.assign( { x: entry[0], y: Math.abs(entry[1]) }));
+
+    const payeePieData =
+    Object.entries(this.state.user.spendingByPayee)
+      .map(entry => Object.assign( { x: entry[0], y: Math.abs(entry[1]) }));
+
+    console.log('spendingPieData is ', spendingPieData, 'payeePieDate is', payeePieData);
+
     return (
       <div className="container">
         <h1 className="title is-1">{this.state.user.username}</h1>
         <h3 className="title is-3">Balance: {this.state.user.balance}</h3>
+        <FlexRowDiv>
+          <PieChartDiv>
+            <h3 className="subtitle is-3 has-text-centered">Spending By Category</h3>
+            <VictoryPie
+              data={spendingPieData}
+              colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy' ]}
+              labels={(d) => d.a}
+              // labelComponent={
+              //   <VictoryLabel
+              //     angle={30}
+              //     capHeight={0.5}
+              //   />}
+              events={[{ target: 'data', eventHandlers: {
+                onClick: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: (props) => {
+                        const fill = props.style && props.style.fill;
+                        return fill === '#c43a31' ? null : { style: { fill: '#c43a31' } };
+                      }
+                    }, {
+                      target: 'labels',
+                      mutation: (props) => {
+                        return props.text === 'clicked' ? null : { text: (d) => d.x };
+                      }
+                    }
+                  ];
+                }
+              }
+              }]}
+            />
+          </PieChartDiv>
+
+          <PieChartDiv>
+            <h3 className="subtitle is-3 has-text-centered">Spending By Payee</h3>
+            <VictoryPie
+              data={payeePieData}
+              colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy' ]}
+              labels={(d) => d.a}
+              // labelComponent={
+              //   <VictoryLabel
+              //     angle={30}
+              //     capHeight={0.5}
+              //   />}
+              events={[{ target: 'data', eventHandlers: {
+                onClick: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: (props) => {
+                        const fill = props.style && props.style.fill;
+                        return fill === '#c43a31' ? null : { style: { fill: '#c43a31' } };
+                      }
+                    }, {
+                      target: 'labels',
+                      mutation: (props) => {
+                        return props.text === 'clicked' ? null : { text: (d) => d.x };
+                      }
+                    }
+                  ];
+                }
+              }
+              }]}
+            />
+          </PieChartDiv>
+        </FlexRowDiv>
+
+
 
       </div>
     );
