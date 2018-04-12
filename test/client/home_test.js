16cb839da57b364d3e2c66c570ac064b7ed03e2c
Fabian Feldberg <feldbergf@gmail.com>
2018-04-12 17:10:01 +0100

    wrote basic tests for Homeroute - classical component

--- test/client/home_test.js
+++ test/client/home_test.js
@@ -8,18 +8,37 @@ import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
 // Components to Test
-import Home from '../src/components/Rivae/HomeRoute';
-import App from '../src/app';
+import Home from '../../src/components/Rivae/HomeRoute';
+// import App from '../../src/app';
 
 
 describe('Home tests', () => {
 
   it('should render', done => {
 
-    const wrapper = shallow(<App />);
-    expect(wrapper.childAt(0)).to.equal();
+    const wrapper = shallow(<Home />);
+    expect(wrapper.children().length).to.equal(2);
     done();
   });
+
+  // // Testing styled components is such a pain
+  // it('should render a Hero Div and a details Div', done => {
+  //
+  //   const wrapper = shallow(<Home />);
+  //   expect(wrapper.childAt(0).text()).to.equal('Rivae');
+  //   expect(wrapper.childAt(1).text()).to.equal('A new way of managing your money');
+  //   done();
+  // });
+
+  it('should render the Apps Title and Subtitle in the hero', done => {
+
+    const wrapper = shallow(<Home />);
+    expect(wrapper.childAt(0).childAt(0).childAt(0).text()).to.equal('Rivae');
+    expect(wrapper.childAt(0).childAt(1).childAt(0).text()).to.equal('A new way of managing your money');
+    done();
+  });
+
+
 
 
 });
